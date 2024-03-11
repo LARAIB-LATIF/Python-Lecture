88c8207c4447d39ec6b958fbdd7bf922ca67d4d0
LARAIB-LATIF <110758947+LARAIB-LATIF@users.noreply.github.com>
2024-03-11 21:26:30 +0500

    Python Lecture
    Assignment No. 2

---
+++ Python Lecture.py
@@ -0,0 +1,35 @@
+
+
+class PthonBasics:
+    def __init__(selfself):
+        pass
+    thislist=("shoes ,bag, uniform")
+    print(thislist)
+    #list method
+    def my_list(selfself):
+        my_list = [6,7,8,9,10]
+    print(my_list)
+    #dictionary method
+    def my_dictonary(self):
+        my_dist = {"name": "jack", "age": 40}
+        print(my_dist)
+    #condition method
+    def my_condition(self):
+        x = 5
+        if x < 8:
+            print ("x is smaller than 8")
+        else:
+            print("x is greater than 8")
+    #loop method
+    def my_loop(self):
+        for i in range(5):
+            print(i)
+    #array method
+    def my_array(self):
+        import array
+        my_array = array. array('i', [6,7,8,9,10])
+        print( my_array)
+        #inner method
+    def inner_method(self):
+        print("inside inner_method")
+
